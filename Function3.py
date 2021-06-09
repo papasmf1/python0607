59ea1e4ce9ad23258c2d06bc761f422425313843
papasmf1 <papasmf1@gmail.com>
2021-06-09 09:42:38 +0900

    4장 연습

---
+++ Function3.py
@@ -0,0 +1,46 @@
+# Function3.py
+#기본값이 있는 경우
+def times(a=10, b=20):
+    return a*b 
+
+#호출
+print( times() )
+print( times(5) )
+print( times(5,6) )
+
+#키워드 인자 방식
+def connectURI(server, port):
+    str = "http://" + server + ":" + port 
+    return str 
+
+#호출
+print( connectURI("credu.com", "80") )
+print( connectURI(port="80", server="credu.com") )
+
+#가변인자(인자의 갯수가 가변적인 상황)
+def union(*ar):
+    result = []
+    for item in ar:
+        for x in item:
+            if x not in result:
+                result.append(x)
+    return result 
+
+#호출
+print( union("HAM","EGG") )
+print( union("HAM","EGG","SPAM") )
+
+
+#정의되지 않은 인자처리(사전식으로 받기)
+def userURIBuilder(server, port, **user):
+    str = "http://" + server + ":" + port + "/?"
+    for key in user.keys():
+        str += key + "=" + user[key] + "&"
+    return str 
+
+#호출
+print( userURIBuilder("credu.com", "80", id="kim", passwd="1234") )
+print( userURIBuilder("credu.com", "80", id="kim", passwd="1234", 
+    name="mike", age="30") )
+
+
